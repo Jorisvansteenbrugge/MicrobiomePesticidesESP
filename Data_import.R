492d86beb255d0168d2c117d99917523afe0fdc7
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2021-04-26 14:19:41 +0200

    data import

--- Data_import.R
+++ Data_import.R
@@ -10,7 +10,7 @@ taxa %<>% tax_table()
 
 asv <- read.csv2("filtered_table.txt", sep = '\t', row.names = 1, header = T, stringsAsFactors = F) %>% otu_table(taxa_are_rows = T)
 
-meta <- read.csv2("Metadata.csv", stringsAsFactors = F, row.names = 1) %>% head %>% sample_data()
+meta <- read.csv2("Metadata.csv", stringsAsFactors = F, row.names = 1) %>% sample_data()
 
 
 asv_table <- phyloseq(taxa, asv, meta)
